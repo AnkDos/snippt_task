f4bfbea576dec5055b44876443da1ab4b3af541e
AnkDos <ankurpan96@gmail.com>
2018-12-23 14:06:29 +0000

    refactor

--- home.php
+++ home.php
@@ -8,6 +8,7 @@ if(!isset($_SESSION['usr'])){
 
 if(isset($_GET['logout'])){
   session_destroy();    
+  header("Location: index.php");
 }
 $sn = $_SESSION['usr'] ;
 $qer = mysqli_query($conn,"select type from users where sno='$sn'");
@@ -18,7 +19,7 @@ if($fet['type'] == "st"){
 if($fet['type'] == "al"){
     include 'alu.php';
 }
-
-
 ob_end_flush();
 ?>
+
+<a href = "?logout">LOG OUT MATE ? </a>
