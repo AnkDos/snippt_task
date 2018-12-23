2a182a11b827480f32e1f0c7363bcba7783c77c7
AnkDos <ankurpan96@gmail.com>
2018-12-23 01:24:59 +0000

    completed login

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+ob_start();
+session_start();
+require 'con.php';
+if(isset($_POST['btn'])){
+$uname = $_POST['uname'];
+$pass = $_POST['pass'];
+$qer = mysqli_query($conn,"select * from users where usern='$uname'");
+$fet = mysqli_fetch_assoc($qer);
+if($uname == $fet['usern'] && $pass == $fet['pass']){
+     header("Location: home.php");
+}
+else{
+     echo "<script>alert('wrong creds mate !')</script>";
+}
+} 
+ob_end_flush();
+?>
+<center>
+    <h1>Please login to continue </h1><br>
+<form method = "post">    
+     
+    <input type = "text" name = "uname" placeholder = "username"/><br> 
+    <input type = "text" name = "pass" placeholder = "password"/><br>
+    <button type = "submit" name = "btn">Login !</button><br> 
+</form>
+</center>
