3eba4ae5e61974afa8968262d9c7e82ec5741ab2
AnkDos <ankurpan96@gmail.com>
2018-12-23 14:07:14 +0000

    completed student part

--- stu.php
+++ stu.php
@@ -5,14 +5,28 @@ $sn = $_SESSION['usr'];
 $qer = mysqli_query($conn,"select * from users where sno = '$sn'");
 $fet = mysqli_fetch_assoc($qer);
 $usern = $fet['usern'];
-//print_r($fet);
-//$type = "st";
-//echo $sn ;
+
+$qs = mysqli_query($conn,"select * from slots where stun = '$usern' AND status='NOTS'");
+$count = mysqli_num_rows($qs);
+
+if(isset($_POST['btn'])){
+    $sl = $_POST['slot'];
+    $dt = $_POST['date'];
+    $trig = mysqli_query($conn,"insert into slots values('','$usern','$sl','$dt','NOTS')") ;
+    echo "<script>alert('Inserted Sucessfully');</script>";
+    
+} 
+
+
 ob_end_flush();
 ?>
 
 <h1> Hello <?php echo $usern ; ?></h1>
 <center>
+    <?php
+    if($count<2){
+        ?>
+    <form method = "post">
     <p> Select your slot mate ! </p>
     <select name = "slot">
         
@@ -28,6 +42,7 @@ ob_end_flush();
                  $date = date("d");
                  $month = date("m");
                  $year = date("Y");
+                 //Prior to 7 days
                  while($i<=7){
                    $nd = $date."/".$month."/".$year;
                 ?>
@@ -49,6 +64,45 @@ ob_end_flush();
                }
              ?>
           </select>
-             
+          
+            <button type = "submit" name = "btn"> Book ! </button>
+          </form>
+    
+             <?php
+    } 
+    else{
+        echo "<h2> Your 2 bookings are already under review, You can book once accepted or rejected by Alumuni </h2>";
+    }
+             ?>
+        
+        <br>
+        <h2> Your Other Booking Status : </h2>
+            <br><p> <b> rj = REJECTED , ac = ACCEPTED , NOTS = NOT SEEN  </b></p>
+        <table border = "1">
+            
+          <tr>
+           <td> Student Name </td>
+           <td> Date </td>
+           <td> Time Slot </td>
+           <td> Status </td> 
+         </tr>    
+            <?php
+            $qers = mysqli_query($conn,"select * from slots where stun = '$usern'");
+             while($fets = mysqli_fetch_array($qers)){
+            ?>
+               <tr>
+                 <td><?php echo $fets['stun'] ; ?></td>
+                 <td><?php echo $fets['slot'] ; ?> </td>
+                 <td><?php echo $fets['date'] ; ?> </td>
+                 <td> <?php echo $fets['status'] ; ?> </td>
+                </tr>
+            
+            
+            <?php
+             }
+            ?>
+                        
+        </table>
+        
         
         </center>
